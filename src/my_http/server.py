9cadcc0d63dca9350eaac2c7279987419e4abe89
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-25 17:48:34 +0300

    chore: fix correct arguments being passed

--- src/my_http/server.py
+++ src/my_http/server.py
@@ -4,7 +4,7 @@ from .router import router
 from .response import response
 
 
-def server_init():
+def server_init(dictionary_of_paths: dict):
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     my_socket.bind(("", 8080))
     my_socket.listen(5)
@@ -13,6 +13,6 @@ def server_init():
         connection_socket, address = my_socket.accept()
         unparsed_data = connection_socket.recv(1024)
         parsed_data = parse(unparsed_data)
-        routed_content = router(parsed_data)
+        routed_content = router(parsed_data, dictionary_of_paths)
         connection_socket.send(response(routed_content))
         connection_socket.close()
