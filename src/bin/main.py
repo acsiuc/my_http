94c38bd0b2aa93885a0580442d697019f5e8f290
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-25 17:49:45 +0300

    feat: add hello/ path handling

--- src/bin/main.py
+++ src/bin/main.py
@@ -1,5 +1,12 @@
 from my_http.server import server_init
 
 
+def hello():
+    return "Hello World"
+
+
+dictionary_of_paths = {"/hello": hello}
+
+
 if __name__ == "__main__":
-    server_init()
+    server_init(dictionary_of_paths)
