786728dfffb56db076a76bd5f94f79460604bea1
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-26 16:34:41 +0300

    feat: add /time path handler

--- src/bin/main.py
+++ src/bin/main.py
@@ -1,11 +1,16 @@
 from my_http.server import server_init
+import datetime
 
 
 def get_hello():
     return "Hello world."
 
 
-dictionary_of_paths = {"/hello": {"GET": get_hello}}
+def get_time():
+    return str(datetime.datetime.now())
+
+
+dictionary_of_paths = {"/hello": {"GET": get_hello}, "/time": {"GET": get_time}}
 
 if __name__ == "__main__":
     server_init(dictionary_of_paths)
