e80dee0ece379f4e45cfde450748d21e3cb7a4c2
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-26 12:52:55 +0300

    feat: change how path and method are being handled together

--- src/bin/main.py
+++ src/bin/main.py
@@ -1,11 +1,11 @@
 from my_http.server import server_init
 
 
-def hello():
-    return "Hello World"
+def get_hello():
+    return "I just wanna fuck."
 
 
-dictionary_of_paths = {"/hello": hello}
+dictionary_of_paths = {"/hello": {"GET": get_hello}}
 
 if __name__ == "__main__":
     server_init(dictionary_of_paths)
--- src/my_http/router.py
+++ src/my_http/router.py
@@ -3,8 +3,12 @@ from .parser import Request
 
 def router(request: Request, dictionary_of_paths: dict) -> str:
     if request.path in dictionary_of_paths:
-        response = dictionary_of_paths[request.path]()
+        methods_dictionary = dictionary_of_paths[request.path]
     else:
         return "Path not found."
+    if request.method in methods_dictionary:
+        response = methods_dictionary[request.method]()
+    else:
+        return "405 Method Not Allowed"
 
     return response
