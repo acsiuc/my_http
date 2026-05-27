412677aeb0269bc6bbcf5dad3dca6ba274eb2a8b
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-27 21:27:28 +0300

    chore(router): now returns Response object

--- src/my_http/router.py
+++ src/my_http/router.py
@@ -1,14 +1,18 @@
 from .parser import Request
+from my_http.response import Response
 
 
-def router(request: Request, dictionary_of_paths: dict) -> str:
+def router(request: Request, dictionary_of_paths: dict) -> Response:
     if request.path in dictionary_of_paths:
         methods_dictionary = dictionary_of_paths[request.path]
     else:
-        return "Path not found."
+        return Response("HTTP/1.1 404 Not Found", [], "Error 404. Not Found")
+
     if request.method in methods_dictionary:
         response = methods_dictionary[request.method]()
     else:
-        return "405 Method Not Allowed"
+        return Response(
+            "HTTP/1.1 405 Method Not Allowed", [], "Error 405. Method Not Allowed"
+        )
 
     return response
