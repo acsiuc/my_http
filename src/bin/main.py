5d5267274ffff9ade3a252201ba33454d0235211
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-05-27 21:28:33 +0300

    chore: structure every method as a http response message

--- src/bin/main.py
+++ src/bin/main.py
@@ -1,16 +1,37 @@
 from my_http.server import server_init
+from pathlib import Path
+from my_http.response import Response
 import datetime
 
 
-def get_hello():
-    return "Hello world."
+def get_hello() -> Response:
+    status_line = "HTTP/1.1 200 OK"
+    body = "Hello World"
+    headers = [
+        f"Date: {datetime.datetime.now()}",
+        "Content-Type: text",
+        f"Content-Length: {len(body)}",
+    ]
+
+    return Response(status_line, headers, body)
 
 
 def get_time():
     return str(datetime.datetime.now())
 
 
-dictionary_of_paths = {"/hello": {"GET": get_hello}, "/time": {"GET": get_time}}
+def get_html():
+    current_path = Path(__file__)
+    html_file_content = current_path.parent.parent.parent / "html_files/index.html"
+    with open(html_file_content, "r") as file:
+        return file.read()
+
+
+dictionary_of_paths = {
+    "/hello": {"GET": get_hello},
+    "/time": {"GET": get_time},
+    "/html": {"GET": get_html},
+}
 
 if __name__ == "__main__":
     server_init(dictionary_of_paths)
