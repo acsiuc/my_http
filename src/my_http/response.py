827c5c5f3fee5370e32b9a51c6747f065fb40946
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-06-01 17:00:49 +0300

    refactor(response): restructure Response class to be OOP oriented with default headers, status_line and encode methods

--- src/my_http/response.py
+++ src/my_http/response.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass, field
+from datetime import timezone
+import datetime
+import json
 
 
 @dataclass
@@ -35,12 +38,22 @@ class Response:
         status_line = f"HTTP/1.1 {self.status.code} {self.status.phrase()}"
         return status_line
 
+    def encode(self) -> bytes:
+        status_line = self.get_status_line() + "\r\n"
+        if isinstance(self.body, dict):
+            body = json.dumps(self.body)
+            if "Content-Type" not in self.headers:
+                self.headers["Content-Type"] = "application/json"
+        else:
+            body = self.body
+        if "Content-Type" not in self.headers:
+            self.headers["Content-Type"] = "text/plain"
+        self.headers["Content-Length"] = len(body)
+        self.headers["Date"] = datetime.datetime.now(timezone.utc).strftime(
+            "%a, %d %b %Y %H:%M:%S GMT"
+        )
+        headers = "\r\n".join(f"{key}: {value}" for key, value in self.headers.items())
 
-# def response(response: Response) -> bytes:
-#     status_line = response.status_line + "\r\n"
-#     headers = "\r\n".join(response.headers)
-#     body = "\r\n\r\n" + response.body
+        http_response = status_line + headers + "\r\n\r\n" + body
 
-#     http_response = status_line + headers + body
-
-#     return http_response.encode()
+        return http_response.encode()
