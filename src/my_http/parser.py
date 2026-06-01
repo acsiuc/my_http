670e00591960b535d82f738c2418aa5123710b2b
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-06-01 19:44:43 +0300

    refactor(parser): restructure Request class and parser() around OOP principles

--- src/my_http/parser.py
+++ src/my_http/parser.py
@@ -1,27 +1,32 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
 class Request:
-    def __init__(self, method, path, headers, body):
-        self.method = method
-        self.path = path
-        self.headers = headers
-        self.body = body
+    method: str
+    path: str = field(default="")
+    headers: list = field(default_factory=list)
+    body: str = field(default="")
 
+    @classmethod
+    def parse(cls, unparsed_data: bytes) -> Request:
+        parsed_data = unparsed_data.decode()
+        split_data = parsed_data.split("\r\n\r\n")
 
-def parse(unparsed_data: bytes) -> Request:
-    parsed_data = unparsed_data.decode()
-    split_data = parsed_data.split("\r\n\r\n")
-    if len(split_data) < 2:
-        raise ValueError
-    request_and_headers = split_data[0].split("\r\n")
-    request = request_and_headers[0].split(" ")
-    if len(request) != 3:
-        raise ValueError
-    method = request[0]
-    path = request[1]
-    headers = request_and_headers[1:]
-    try:
+        if len(split_data) < 2:
+            raise ValueError
+
+        request_and_headers = split_data[0].split("\r\n")
+        request = request_and_headers[0].split(" ")
+
+        if len(request) != 3:
+            raise ValueError
+
+        method = request[0]
+        path = request[1]
+        headers = request_and_headers[1:]
         body = split_data[1]
-    except IndexError:
-        body = ""
-    object_to_return = Request(method, path, headers, body)
 
-    return object_to_return
+        obj = cls(method, path, headers, body)
+
+        return obj
