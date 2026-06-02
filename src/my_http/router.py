3cee7673731050e7d878d10a57d3dac003b3cbb9
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-06-02 13:13:30 +0300

    refactor: change structure to respect OOP

--- src/my_http/router.py
+++ src/my_http/router.py
@@ -1,48 +1,40 @@
 from .parser import Request
-from my_http.response import Response
+from my_http.response import Response, NotFound, NotAllowed
+from typing import Callable
+from dataclasses import dataclass, field
 from pathlib import Path
-import datetime
-import mimetypes
-
-dictionary_of_paths = {}
 
 
-def route(path, method):
-    def write_to_dict(func):
-        if path not in dictionary_of_paths:
-            dictionary_of_paths[path] = {method: func}
+@dataclass
+class App:
+    dictionary_of_paths: dict[str, dict[str, Callable]] = field(default_factory=dict)
+
+    def route(self, path, method):
+        def write_to_dict(func):
+            if path not in self.dictionary_of_paths:
+                self.dictionary_of_paths[path] = {method: func}
+            else:
+                self.dictionary_of_paths[path][method] = func
+            return func
+
+        return write_to_dict
+
+    def router(self, request: Request) -> Response:
+        current_path = Path(__file__)
+        path = current_path.parent.parent.parent / f"html_files{request.path}"
+        path_exists = Path.is_file(path)
+
+        if request.path in self.dictionary_of_paths:
+            methods_dictionary = self.dictionary_of_paths[request.path]
+        elif path_exists:
+            with open(path, "r") as f:
+                body = f.read()
+            return Response(body=body)
         else:
-            dictionary_of_paths[path][method] = func
-        return func
+            return Response(status=NotFound(), body="Error 404. Not Found")
 
-    return write_to_dict
-
-
-def router(request: Request, dictionary_of_paths: dict) -> Response:
-    current_path = Path(__file__)
-    path = current_path.parent.parent.parent / f"html_files{request.path}"
-    path_exists = Path.is_file(path)
-    if request.path in dictionary_of_paths:
-        methods_dictionary = dictionary_of_paths[request.path]
-    elif path_exists:
-        with open(path, "r") as f:
-            body = f.read()
-        status_line = "HTTP/1.1 200 OK"
-        file_type = mimetypes.guess_type(path)[0] or "text/plain"
-        headers = [
-            f"Date: {datetime.datetime.now()}",
-            f"Content-Type: {file_type}",
-            f"Content-Length: {len(body)}",
-        ]
-        return Response(status_line, headers, body)
-    else:
-        return Response("HTTP/1.1 404 Not Found", [], "Error 404. Not Found")
-
-    if request.method in methods_dictionary:
-        response = methods_dictionary[request.method](request)
-    else:
-        return Response(
-            "HTTP/1.1 405 Method Not Allowed", [], "Error 405. Method Not Allowed"
-        )
-
-    return response
+        if request.method in methods_dictionary:
+            response = methods_dictionary[request.method](request)
+            return response
+        else:
+            return Response(status=NotAllowed(), body="Error 405. Method Not Allowed")
