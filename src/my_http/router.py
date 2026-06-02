4007111d126584e68ef5199d0a9951d793e632a5
Axiuc Fedor <florinaxiuc22@gmail.com>
2026-06-02 19:19:15 +0300

    chore(router): separate file concerns from routing

--- src/my_http/router.py
+++ src/my_http/router.py
@@ -8,6 +8,7 @@ from pathlib import Path
 @dataclass
 class App:
     dictionary_of_paths: dict[str, dict[str, Callable]] = field(default_factory=dict)
+    static_file: str = field(default=None)
 
     def route(self, path, method):
         def write_to_dict(func):
@@ -19,22 +20,28 @@ class App:
 
         return write_to_dict
 
+    def send_static_file(self, filename):
+        if not self.static_file:
+            raise RuntimeError("static_files must be set")
+        with open(Path(self.static_file) / filename, "r") as f:
+            body = f.read()
+        return Response(body=body)
+
     def router(self, request: Request) -> Response:
-        current_path = Path(__file__)
-        path = current_path.parent.parent.parent / f"html_files{request.path}"
-        path_exists = Path.is_file(path)
+        path_exists = Path.is_file(Path(self.static_file) / request.path)
 
         if request.path in self.dictionary_of_paths:
             methods_dictionary = self.dictionary_of_paths[request.path]
         elif path_exists:
-            with open(path, "r") as f:
-                body = f.read()
-            return Response(body=body)
+            return self.send_static_file(request.path)
         else:
             return Response(status=NotFound(), body="Error 404. Not Found")
 
         if request.method in methods_dictionary:
             response = methods_dictionary[request.method](request)
-            return response
+            if isinstance(response, Response):
+                return response
+            else:
+                return Response(body=response)
         else:
             return Response(status=NotAllowed(), body="Error 405. Method Not Allowed")
